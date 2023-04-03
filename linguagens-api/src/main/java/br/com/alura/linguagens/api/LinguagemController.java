ec552307f69d250ca2007fd4dc9b48a5607d5b00
matheus-stein-nassr <ingridsnassr@hotmail.com>
2023-04-02 21:21:07 -0300

    CRUD Linguagens

--- linguagens-api/src/main/java/br/com/alura/linguagens/api/LinguagemController.java
+++ linguagens-api/src/main/java/br/com/alura/linguagens/api/LinguagemController.java
@@ -1,13 +1,17 @@
 package br.com.alura.linguagens.api;
 
-import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 
 @RestController
@@ -28,4 +32,25 @@ public class LinguagemController {
         return linguagemSalva;
     } 
 
+    @GetMapping("/linguagens/{id}")
+    public Linguagem obterLinguagemPorId(@PathVariable String id){
+        return repositorio.findById(id)
+            .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    @PutMapping("/linguagens/{id}")
+    public Linguagem atualizarLinguagem(@PathVariable String id, @RequestBody Linguagem linguagem){
+        if (!repositorio.existsById(id)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        linguagem.setId(id);
+        Linguagem linguagemSalva = repositorio.save(linguagem);
+        return linguagemSalva;
+    }
+
+    @DeleteMapping("/linguagens/{id}")
+    public void deletarLinguagem(@PathVariable String id){
+        repositorio.deleteById(id);
+    }
+    
 }
